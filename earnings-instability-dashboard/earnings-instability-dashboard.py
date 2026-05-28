bc230ddfe353818083d006613feb675fb9e3974d
rob-encore <rob@encoregm.com>
2026-05-28 08:13:06 -0500

    Update earnings-instability-dashboard.py

--- earnings-instability-dashboard/earnings-instability-dashboard.py
+++ earnings-instability-dashboard/earnings-instability-dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import psycopg2
-import plotly.express 
+import plotly.express as px
 import plotly.graph_objects as go
 
 # =========================================================
